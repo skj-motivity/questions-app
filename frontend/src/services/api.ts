f2527bb20e828383f2a1a6fb54e36cd8b7db5d99
AnuragSinghML <anurag.singh@motivitylabs.com>
2026-02-20 13:00:34 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: `${import.meta.env.VITE_STRAPI_API_URL || "http://localhost:1337"}/api`,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_STRAPI_API_TOKEN}`,
+  },
 });
